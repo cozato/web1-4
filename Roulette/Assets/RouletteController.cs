86d9c44fb65599ba7b79bee4628dbdf07cd99707
tkskd <g1727038@st.t-kougei.ac.jp>
2018-05-21 17:59:53 +0900

    add_moveScript

---
+++ Roulette/Assets/RouletteController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class RouletteController : MonoBehaviour {
+
+    float rotSpeed = 0;//回転速度
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //マウスが押されたら回転速度を設定する
+        if (this.rotSpeed < 0.1f)
+        {
+            this.rotSpeed = 0;
+        }
+
+        if(Input.GetMouseButtonDown(0))
+        {
+            this.rotSpeed += 10;
+        }
+
+        //回転速度分、ルーレットを回転させる
+        transform.Rotate(0, 0, this.rotSpeed);
+        //ルーレットを減速させる
+        this.rotSpeed *= 0.96f;
+	}
+}
